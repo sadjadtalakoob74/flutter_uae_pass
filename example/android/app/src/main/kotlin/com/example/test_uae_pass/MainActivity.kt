5289baa30ff6287e36c1aaa459abc7923684b7dc
mabdo <mohamed_abdo95@outlook.com>
2025-02-05 13:16:57 +0400

    v 1.0.1

---
+++ example/android/app/src/main/kotlin/com/example/test_uae_pass/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.test_uae_pass
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
--- example/android/app/src/main/kotlin/com/mvpapps/uae_pass_example/MainActivity.kt
+++
@@ -1,7 +0,0 @@
-package com.mvpapps.uae_pass_example
-
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
